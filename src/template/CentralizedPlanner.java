c5fe37d50c7b3ed3c28852be6b67137ce93787b3
samsara <chong.han@epfl.ch>
2015-11-06 17:31:45 +0100

    added removeJob() and selectInitialSolution()

--- src/template/CentralizedPlanner.java
+++ src/template/CentralizedPlanner.java
@@ -1,12 +1,15 @@
 package template;
 
 import logist.plan.Action;
+import logist.plan.Action.Delivery;
+import logist.plan.Action.Pickup;
 import logist.plan.Plan;
 import logist.simulation.Vehicle;
 import logist.task.Task;
 import logist.task.TaskSet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -25,6 +28,65 @@ public class CentralizedPlanner
     {
         this.jobList = new ArrayList<LinkedList<Job>>(vehicles.size());
         this.vehicles = vehicles;
+
+        selectInitialSolution(tasks);
+    }
+
+
+    /**
+     * Remove both pickup and delivery of a task from jobList
+     *
+     * @param vehicle vehicle number
+     * @param t       task to be removed
+     */
+    private void removeJob(int vehicle, Task t)
+    {
+        Iterator<Job> iterator = jobList.get(vehicle).listIterator();
+        while (iterator.hasNext())
+        {
+            Job j = iterator.next();
+            if (j.getT().equals(t))
+            {
+                iterator.remove(); // Remove pickup and delivery
+            }
+        }
+
+    }
+
+    /**
+     * Give all the tasks to the biggest vehicle. If there exist some tasks that do not fit for the vehicle, then the
+     * problem is unsolvable.
+     */
+    public void selectInitialSolution(TaskSet tasks)
+    {
+        // Find biggest vehicle
+        int vehicleId = 0;
+        int capacity = 0;
+        for (Vehicle v : vehicles)
+        {
+            int temp;
+            if ((temp = v.capacity()) > capacity)
+            {
+                vehicleId = v.id();
+                capacity = temp;
+            }
+        }
+
+        LinkedList<Job> jobs = jobList.get(vehicleId);
+        for (Task t : tasks)
+        {
+            if (t.weight < capacity)
+            {
+                jobs.add(new Job(t, new Pickup(t)));
+                jobs.add(new Job(t, new Delivery(t)));
+            } else
+            {
+                throw new IllegalArgumentException("Task do not fit any vehile");
+            }
+
+        }
+
+
     }
 
     // TODO
@@ -56,10 +118,11 @@ public class CentralizedPlanner
     {
         return 0;
     }
+
     private class Job
     {
-        private Task t;
-        private Action a;
+        private final Task t;
+        private final Action a;
 
         public Job(Task t, Action a)
         {
