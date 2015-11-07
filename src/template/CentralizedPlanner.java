aa97ea84499f930c938a716bb980d9e53b506a3b
samsara <chong.han@epfl.ch>
2015-11-07 03:55:42 +0100

    late commit

--- src/template/CentralizedPlanner.java
+++ src/template/CentralizedPlanner.java
@@ -1,12 +1,10 @@
 package template;
 
-import logist.plan.Action;
-import logist.plan.Action.Delivery;
-import logist.plan.Action.Pickup;
 import logist.plan.Plan;
 import logist.simulation.Vehicle;
 import logist.task.Task;
 import logist.task.TaskSet;
+import logist.topology.Topology.City;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -18,46 +16,72 @@ import java.util.List;
  */
 public class CentralizedPlanner
 {
-    // V0 Job(Task, Action), Job(Task, Action)
-    // V1 Job(Task, Action), ...
-    // ...
-    private ArrayList<LinkedList<Job>> jobList;
-    private List<Vehicle> vehicles;
+    public static final int PICKUP = 0;
+    public static final int DELIVERY = 1;
 
+    private static Task[] tasks;
+    private static List<Vehicle> vehicles;
+
+    private ArrayList<LinkedList<Job>> jobList; // V0 Job(Task, Action), Job(Task, Action)
+
+    // V1 Job(Task, Action), ...
+    // V2 ...
     public CentralizedPlanner(List<Vehicle> vehicles, TaskSet tasks)
     {
         this.jobList = new ArrayList<LinkedList<Job>>(vehicles.size());
-        this.vehicles = vehicles;
+        CentralizedPlanner.tasks = (Task[]) tasks.toArray();
+        CentralizedPlanner.vehicles = vehicles;
 
-        selectInitialSolution(tasks);
+        selectInitialSolution();
     }
-
 
     /**
      * Remove both pickup and delivery of a task from jobList
      *
      * @param vehicle vehicle number
-     * @param t       task to be removed
+     * @param task    task to be removed
      */
-    private void removeJob(int vehicle, Task t)
+    private static void removeJob(ArrayList<LinkedList<Job>> jobList, int vehicle, int task)
     {
         Iterator<Job> iterator = jobList.get(vehicle).listIterator();
         while (iterator.hasNext())
         {
             Job j = iterator.next();
-            if (j.getT().equals(t))
+            if (j.getT() == task)
             {
                 iterator.remove(); // Remove pickup and delivery
             }
         }
+    }
 
+    private static double computeCost(LinkedList<Job> jobs, Vehicle vehicle)
+    {
+        Task t;
+        double distance = 0;
+        City homeCity = vehicle.homeCity();
+        for (Job j : jobs)
+        {
+            t = tasks[j.getT()];
+            City taskCity;
+            if (j.getA() == PICKUP)
+            {
+                taskCity = t.pickupCity;
+                distance += taskCity.distanceTo(homeCity);
+            } else
+            {
+                taskCity = t.deliveryCity;
+                distance += taskCity.distanceTo(homeCity);
+            }
+            homeCity = taskCity;
+        }
+        return distance * vehicle.costPerKm();
     }
 
     /**
      * Give all the tasks to the biggest vehicle. If there exist some tasks that do not fit for the vehicle, then the
      * problem is unsolvable.
      */
-    public void selectInitialSolution(TaskSet tasks)
+    public void selectInitialSolution()
     {
         // Find biggest vehicle
         int vehicleId = 0;
@@ -72,21 +96,19 @@ public class CentralizedPlanner
             }
         }
 
+        // Add all tasks to one vehicle
         LinkedList<Job> jobs = jobList.get(vehicleId);
-        for (Task t : tasks)
+        for (int i = 0; i < tasks.length; i++)
         {
-            if (t.weight < capacity)
+            if (tasks[i].weight < capacity)
             {
-                jobs.add(new Job(t, new Pickup(t)));
-                jobs.add(new Job(t, new Delivery(t)));
+                jobs.add(new Job(i, PICKUP));
+                jobs.add(new Job(i, DELIVERY));
             } else
             {
-                throw new IllegalArgumentException("Task do not fit any vehile");
+                throw new IllegalArgumentException("Task do not fit any vehicle");
             }
-
         }
-
-
     }
 
     // TODO
@@ -119,27 +141,37 @@ public class CentralizedPlanner
         return 0;
     }
 
-    private class Job
+    private class Job implements Cloneable
     {
-        private final Task t;
-        private final Action a;
+        private final int t; // index of the task in tasks array
+        private final int a; // PICKUP or DELIVERY
 
-        public Job(Task t, Action a)
+        public Job(int t, int a)
         {
             this.t = t;
             this.a = a;
         }
 
-        public Task getT()
+        public Job(Job j)
+        {
+            this.t = j.getT();
+            this.a = j.getA();
+        }
+
+        public int getT()
         {
             return t;
         }
 
-        public Action getA()
+        public int getA()
         {
             return a;
         }
 
-
+        @Override
+        protected Object clone() throws CloneNotSupportedException
+        {
+            return super.clone();
+        }
     }
 }
